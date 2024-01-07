7c0e7c9d0ac77ed1cf6c6e7bec7509dcd612f00e
Jinseo Park <73384343+JinseoPark-bd@users.noreply.github.com>
2024-01-07 21:46:35 +0900

    Title: 행복한 수열의 개수, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240107/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][n];
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            int s = 1;
+            for(int j = 0; j < n-1; j++) {
+                if(arr[i][j] == arr[i][j+1]) s++;
+                else {
+                    s = 1;
+                    continue;
+                }
+
+                if(s == m) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            int s = 1;
+            for(int j = 0; j < n-1; j++) {
+                if(arr[j][i] == arr[j+1][i]) s++;
+                else {
+                    s = 1;
+                    continue;
+                }
+
+                if(s == m) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}

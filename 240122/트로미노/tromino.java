3322c43fefc19005a9514dba76f05a4f1068128c
Jinseo Park <73384343+JinseoPark-bd@users.noreply.github.com>
2024-01-22 16:10:37 +0900

    Title: 트로미노, Time: 450ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/트로미노/tromino.java
+++ 240122/트로미노/tromino.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
+
 public class Main {
     public static final int MAX_NUM = 200;
-    public static int n,m;
+    
+    public static int n, m;
     public static int[][] grid = new int[MAX_NUM][MAX_NUM];
-    public static int[][][] shapes = new int[][][] {
+    
+    // 가능한 모든 모양을 전부 적어줍니다.
+    public static int[][][] shapes = new int[][][]{
         {{1, 1, 0},
         {1, 0, 0},
         {0, 0, 0}},
@@ -28,44 +32,45 @@ public class Main {
         {1, 0, 0},
         {1, 0, 0}},
     };
-
+    
+    // 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환합니다.
     public static int getMaxSum(int x, int y) {
         int maxSum = 0;
+        
         for(int i = 0; i < 6; i++) {
             boolean isPossible = true;
             int sum = 0;
-            for(int dx = 0; dx < 3; dx++) {
+            for(int dx = 0; dx < 3; dx++)
                 for(int dy = 0; dy < 3; dy++) {
                     if(shapes[i][dx][dy] == 0) continue;
-                    if(x + dy >= n || y + dy >= m) isPossible = false;
-                    else sum += grid[x+dx][y+dy];
+                    if(x + dx >= n || y + dy >= m) isPossible = false;
+                    else sum += grid[x + dx][y + dy];
                 }
-            }
+    
             if(isPossible)
                 maxSum = Math.max(maxSum, sum);
         }
+        
         return maxSum;
     }
 
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
         n = sc.nextInt();
         m = sc.nextInt();
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
                 grid[i][j] = sc.nextInt();
-            }
-        }
-
+        
         int ans = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                ans = Math.max(ans, getMaxSum(i,j));
-            }
-        }
-
-        System.out.println(ans);
+        
+        // 격자의 각 위치에 대하여 탐색하여줍니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                ans = Math.max(ans, getMaxSum(i, j));
+        
+        System.out.print(ans);
     }
 }

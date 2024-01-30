ed68a4a6559c89af931bbb5763895b689243a80e
Jinseo Park <73384343+JinseoPark-bd@users.noreply.github.com>
2024-01-30 17:45:12 +0900

    Title: 아름다운 수, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240130/아름다운 수/beautiful-number.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main {
+    public static int n;
+    public static int count = 0;
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static int answer = 0;
+    /**
+    * 1이상 4이하의 수로만
+    * 아름다운수 : 숫자가 숫자개수만큼 연달아 있는 경우
+    **/
+    public static void beautifulNum(int s) {
+        answer += s;
+
+        if(answer == n){
+            count++;
+            return;
+        }
+
+        if(answer > n) {
+            answer -= s;
+            return;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            beautifulNum(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        beautifulNum(0);
+        System.out.print(count);
+    }
+}

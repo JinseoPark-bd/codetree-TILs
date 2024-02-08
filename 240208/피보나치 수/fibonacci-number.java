d9dde845e6a6ad9ad703b1c0cef784ec1fd6a7f9
Jinseo Park <73384343+JinseoPark-bd@users.noreply.github.com>
2024-02-08 22:51:00 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240208/피보나치 수/fibonacci-number.java
+++ 240208/피보나치 수/fibonacci-number.java
@@ -9,17 +9,14 @@ public class Main {
     public static int N;
     public static final int MAX_N = 45;
     public static int[] dp = new int[MAX_N];
-    public static int fibo(int n) {
-        for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         dp[1] = 1;
         dp[2] = 1;
-        System.out.print(fibo(N));
+        for(int i = 3; i <= N; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.print(dp[N]);
     }
 }

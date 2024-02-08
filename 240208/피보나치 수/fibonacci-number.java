c036bbbab3919e3ad6115714162873a11f3abb3e
Jinseo Park <73384343+JinseoPark-bd@users.noreply.github.com>
2024-02-08 22:18:09 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240208/피보나치 수/fibonacci-number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    /**
+    dp : 큰 문제에 대한 답을 얻기 위해 동일한 문제이지만 크기가 더 작은 문제들을 먼저 해결한 뒤, 그 결과들을 이용해 큰 문제를 비교적 간단하게 해결하는 기법
+    메모이제이션 : 값을 기록하고, 그 기록한 값을 참조하는 것
+    Tabulation : 아래에서 값을 채워 나가기 때문에 바텀업 방식 (Bottom-Up Approach) 라고 부릅니다. 바텀업 방식이 약간 더 빠릅니다.
+    **/
+    public static int N;
+    public static final int MAX_N = 45;
+    public static int[] dp = new int[MAX_N];
+    public static int fibo(int n) {
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        dp[1] = 1;
+        dp[2] = 1;
+        System.out.print(fibo(N));
+    }
+}
